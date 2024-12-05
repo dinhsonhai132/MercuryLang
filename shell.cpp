56ed61bceb0f0fa3a56b5ffa8dc6dba4b497783f
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2024-12-05 18:17:49 +0700

    shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -10,12 +10,15 @@ enum VerType {
 struct datatype {
     VerType type;
     int value;
+    string name;
 };
 
-struct var {
+struct store_var {
     string name;
     int val;
 };
+
+vector<store_var> varibles;
 
 class lexer {
 private:
@@ -23,41 +26,81 @@ private:
     size_t pos;
     char cur;
     vector<datatype> tokens;
-    vector<var> vars;
-    size_t tok_idx;
-    datatype cur_idx;
 public:
-    lexer(string input) : input(input), pos(0), cur(input[pos]), tok_idx(0) {}
+    lexer(string input) : input(input), pos(0) {}
+    
+    void advance() {
+        if (pos < input.size()) {
+            pos++;
+            cur = input[pos];
+        } else {
+            cur = '\0';
+        }
+    }
 
     vector<datatype> token() {
-        datatype datatoken;
-        int num = 0;
+
         while (pos < input.size()) {
             cur = input[pos];
             if (cur == '+') {
-                tokens.push_back({PLUS, 0});
+                tokens.push_back({PLUS, 0, ""});
+                advance();
             } else if (cur == '-') {
-                tokens.push_back({MINUS, 0});
+                tokens.push_back({MINUS, 0, ""});
+                advance();
             } else if (cur == '*') {
-                tokens.push_back({TIME, 0});
+                tokens.push_back({TIME, 0, ""});
+                advance();
             } else if (cur == '/') {
-                tokens.push_back({DIV, 0});
+                tokens.push_back({DIV, 0, ""});
+                advance();
             } else if (isdigit(cur)) {
-                num = 0;
+                int num = 0;
                 while (isdigit(input[pos]) && pos < input.size()) {
                     num = num * 10 + (input[pos] - '0');
-                    pos++;
+                    advance();
                 }
-                tokens.push_back({INT, num});
-                continue;
+                tokens.push_back({INT, num, ""});
             } else if (cur == ';') {
-                tokens.push_back({NONE, 0});
+                tokens.push_back({NONE, 0, ""});
+                advance();
+            } else if (cur == '@') {
+                int val = 0;
+                string name = "";
+                while (pos < input.size()) {
+                    if (isalpha(input[pos])) {
+                        name += input[pos];
+                    } else if (input[pos] == '=') {
+                        break;
+                    }
+                    advance();
+                }
+                while (pos < input.size()) {
+                    if (isdigit(input[pos])) {
+                        val = val * 10 + (input[pos] - '0');
+                    } else if (input[pos] == '=') {
+                        break;
+                    }
+                    advance();
+                }
+                varibles.push_back({name, val});
+            } else if (isalpha(cur)) {
+                string name = "";
+                while (isalpha(cur)) {
+                    name += input[pos];
+                    advance();
+                }
+                tokens.push_back({VAR, 0, name});
+            } else if (isspace(cur)) {
+                advance();
+            } else {
+                advance();
             }
-            pos++;
         }
         return tokens;
     }
 };
+
 
 class parser {
 private:
@@ -69,64 +112,99 @@ private:
 public:
     parser(vector<datatype> tokenize) : tokenize(tokenize), tok_idx(0) {}
 
-    int eval() {
-        auto get_next_tok = [&]() -> datatype {
+    datatype get_next_tok() {
+        if (tok_idx < tokenize.size()) {
             return tokenize[tok_idx++];
-        };
+        }
+        return {NONE, 0, ""};
+    }
 
-        auto factor = [&]() -> int {
+    int factor() {
+        cur_idx = get_next_tok();
+        if (cur_idx.type == INT) {
+            return cur_idx.value;
+        } else if (cur_idx.type == VAR) {
+            string var_name = cur_idx.name;
+            for (auto &varible : varibles) {
+                if (varible.name == var_name) {
+                    return varible.val;
+                }
+            }
+            cerr << "Variable not found: " << var_name << endl;
+            return 0;
+        } 
+        cerr << "Unexpected token in factor: " << cur_idx.type << endl;
+        return 0;
+    }
+
+    int term() {
+        int result = factor();
+        while (true) {
             cur_idx = get_next_tok();
-            if (cur_idx.type == INT) {
-                return cur_idx.value;
-            }
-        };
-
-        auto term = [&]() -> int {
-            int result = factor();
-            while (true) {
-                cur_idx = get_next_tok();
-                if (cur_idx.type == DIV) {
-                    result /= factor();
-                } else if (cur_idx.type == TIME) {
-                    result *= factor();
-                } else {
-                    tok_idx--;
-                    break;
+            if (cur_idx.type == DIV) {
+                int divisor = factor();
+                if (divisor == 0) {
+                    cerr << "Error: Division by zero" << endl;
+                    return 0;
                 }
+                result /= divisor;
+            } else if (cur_idx.type == TIME) {
+                result *= factor();
+            } else {
+                tok_idx--;
+                break;
             }
-            return result;
-        };
+        }
+        return result;
+    }
 
-        auto expr = [&]() -> int {
-            int result = term();
-            while (true) {
-                cur_idx = get_next_tok();
-                if (cur_idx.type == PLUS) {
-                    result += term();
-                } else if (cur_idx.type == MINUS) {
-                    result -= term();
-                } else {
-                    tok_idx--;
-                    break;
-                }
+    int expr() {
+        int result = term();
+        while (true) {
+            cur_idx = get_next_tok();
+            if (cur_idx.type == PLUS) {
+                result += term();
+            } else if (cur_idx.type == MINUS) {
+                result -= term();
+            } else {
+                tok_idx--;
+                break;
             }
-            return result;
-        };
-        
-        return expr();
+        }
+        return result;
+    }
+ 
+
+    void print_var() {
+        for (auto &varible: varibles) {
+            cout << varible.name << "=" << varible.val << endl;
+        }
     }
 };
 
-int main() {
-    cout << "Mercury [Version 0.0.1] \n(c) All rights reserved." << endl;
+void run() {
+    cout << "fslang2 [Version 0.0.2] \n(c) (this is test version) All rights reserved.\n type 'help?' for help" << endl;
     while (true) {
         string input;
         cout << ">>> ";
         getline(cin, input);
-        lexer lex = lexer(input);
+        if (input.empty()) {
+            cout << endl;
+            continue;
+        }
+
+        lexer lex(input);
         vector<datatype> tok = lex.token();
-        parser par = parser(tok);
-        int result = par.eval();
-        cout << result << endl;
+        parser par(tok);
+
+        if (input == "help?") {
+            cout << "Visit https://dinhsonhai132.github.io/fslang.github.io/fslang.html for more info" << endl;
+        } else {
+            cout << par.expr() << endl;
+        }
     }
+}
+
+int main() {
+    run();
 }

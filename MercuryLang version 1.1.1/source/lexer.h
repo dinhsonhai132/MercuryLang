c2f39cece2e01f9c1fb6ab90dae85a8987eeea19
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-01-12 22:50:48 +0700

    Update lexer.h

--- MercuryLang version 1.1.1/source/lexer.h
+++ MercuryLang version 1.1.1/source/lexer.h
@@ -6,6 +6,52 @@
 #include "parser.h"
 using namespace std;
 
+enum VerType {
+    INT, PLUS, MINUS, TIME, DIV, NONE, MEMORY, PRINT, STRING, 
+    TEMPORARY_MEMORY, BIGGER, SMALLER, EQUAL, BE, SE, DIFFERENCES, IF, ELSE, ELIF, PARAMATER_KWARGS,
+    THEN, LP, RP, FOR, PP, MM, WHILE, LET, ASSIGN, GOTO, INPUT, LIST, BLOCK, RETURN_FUNC,
+    FUNCTION, PARAMATER, FUNCTION_CALL, COMMA, DOUBLE_COLON, COMMAND, CIN, COMMAND_START, COMMAND_END,
+    DO, VECTOR, SPARE_LP, SPARE_RP, LIST_NAME, EXTRACT, RANGE, FOR_LOOP, IN, TO, END
+};
+
+enum Mercury_type {
+    INT_TYPE, FLOAT_TYPE, STRING_TYPE
+};
+
+struct store_var {
+    string name;
+    int val;
+};
+
+struct Parameter {
+    string name;
+    int val;
+};
+
+struct Parameter_kwargs {
+    string name;
+    vector<int> vec;
+};
+
+struct datatype {
+    VerType type;
+    int value;
+    string name;
+};
+
+struct store_list {
+    string name;
+    vector<int> list;
+};
+
+struct function {
+    string function_name;
+    vector<Parameter> Parameters;
+    vector<datatype> store_tokens;
+    vector<Parameter_kwargs> parameter_kwargs;
+    int value = 0;
+};
+
 class lexer {
 
 private:
@@ -13,6 +59,7 @@ private:
     size_t pos;
     char cur;
     vector<datatype> tokens;
+    vector<Parameter> paaaas;
     
 public:
     void advance();

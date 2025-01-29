ff83402c64ff601383afb183531a410157454d3d
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-01-29 16:58:11 +0700

    Add files via upload

---
+++ 3.0.1 test/MerCore.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MerCoreToken.h"
+#include "MerCoreDataType.h"
+#include "MerState.h"
+
+typedef ssize_t Mer_ssize_t;
+typedef int Mer_int;
+typedef float Mer_float;
+
+#ifndef MAX_LIST_INPUT => 1 && MAX_LIST_INPUT <= 1000;
+    #define MAX_LIST_INPUT 1000;
+    #define MIN_LIST_INPUT 1;
+#endif
---
+++ 3.0.1 test/MerCoreDataType.h
@@ -0,0 +1,9 @@
+#ifndef MER_CORE_DATA_TYPE_H
+#define MER_CORE_DATA_TYPE_H
+#include "MerCore.h"
+
+typedef enum MER_DATA_TYPE {
+    FLOAT_TYPE, INT_TYPE, STRING_TYPE, NONE_TYPE
+} data_type;
+
+#endif

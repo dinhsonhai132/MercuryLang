3f0cbc160703db0415568c0c064c927d48d3cd64
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-01-30 18:51:47 +0700

    Delete 3.0.1 test directory

--- 3.0.1 test/MerCoreDataType.h
+++
@@ -1,9 +0,0 @@
-#ifndef MER_CORE_DATA_TYPE_H
-#define MER_CORE_DATA_TYPE_H
-#include "MerCore.h"
-
-typedef enum MER_DATA_TYPE {
-    FLOAT_TYPE, INT_TYPE, STRING_TYPE, NONE_TYPE
-} data_type;
-
-#endif
--- 3.0.1 test/MerState.cpp
+++
@@ -1,1 +0,0 @@
-#include "MerState.h"

e47b560f3e3ea102014e25753ebf0f44531fa60e
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-01-30 20:40:00 +0700

    Delete 3.0.1 test directory

--- 3.0.1 test/Error/MerErrorIden.h
+++
@@ -1,8 +0,0 @@
-#ifndef MER_ERROR_IDEN_H
-#define MER_ERROR_IDEN_H
-
-#define ERROR_UNEXPECTED_FACTOR         0
-#define ERROR_DATA_OVER_FLOW            1
-#define ERROR_OUT_OF_STACK              2
-
-#endif
--- 3.0.1 test/MerCoreError.h
+++
@@ -1,8 +0,0 @@
-#ifndef MER_CORE_ERROR_H
-#define MER_CORE_ERROR_H
-#include "MerErrorIden.h"
-#include "MerState.h"
-
-static int *Mer_Core_Error_token_out_of_stack(tok_state *tok);
-
-#endif

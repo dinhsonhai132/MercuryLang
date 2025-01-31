9728c29637de4dae4c7a6957f7e90927c7873496
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-01-31 15:20:11 +0700

    Delete Test/2.0.1 test directory

--- Test/2.0.1 test/parser/lexer/MerLexer.h
+++
@@ -1,9 +0,0 @@
-#include "MerCore.h"
-#include "MerState.h"
-#ifndef MER_LEXER_H
-#define MER_LEXER_
-
-int MerLexer_getn_char(char_state *cur);
-int MerLexer_getn_tok(char_state *cur, tok_state *tok);
-
-#endif

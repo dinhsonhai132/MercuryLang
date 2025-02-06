bd2f2a91ce2973d54aba0f625251401d6d8d63e6
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-02-06 11:55:49 +0700

    Delete test directory

--- test/Source/Core/error.cpp
+++
@@ -1,6 +0,0 @@
-#include "error.h"
-
-Mer_RuntimeError::Mer_RuntimeError(const string& msg) : fullMsg("Runtime Error: " + msg) {}
-const char *Mer_RuntimeError::what() const throw() { 
-    return fullMsg.c_str();
-}

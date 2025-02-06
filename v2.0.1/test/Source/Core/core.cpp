4975a8d78b2211ee6076ec24cbcb16c6b7531d12
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-02-06 21:28:29 +0700

    Update core.cpp

--- v2.0.1/test/Source/Core/core.cpp
+++ v2.0.1/test/Source/Core/core.cpp
@@ -22,8 +22,8 @@ RunTimeVal evaluate_binary_expr(AST_BinaryExpression binexpr, Environment& envi)
 }
 
 RunTimeVal eval_num_expr(RunTimeVal left, RunTimeVal right, string op_tok) {
-    NumberVal left_num = left;
-    NumberVal right_num = right;
+    NumberVal left_num = static_cast<NumberVal&>(left);
+    NumberVal right_num = static_cast<NumberVal&>(right);
 
     if (op_tok == PLUS) {
         return NumberVal {
@@ -55,16 +55,16 @@ RunTimeVal evaluate_identifier(AST_Identifier iden, Environment& envi) {
 
 RunTimeVal evaluate(statement state, Environment& envi) {
     if (state.type == Literal) {
-        AST_Literal li = state;
+        AST_Literal li = static_cast<AST_Literal&>(state);
         return NumberVal {
             .type = _Number,
             .value = li.value
         };
     } else if (state.type == BinaryExpression) {
-        AST_BinaryExpression bin = state;
+        AST_BinaryExpression bin = static_cast<AST_BinaryExpression&>(state);
         return evaluate_binary_expr(bin, envi);
     } else if (state.type == Program) {
-        AST_Program prog = state;
+        AST_Program prog = static_cast<AST_Program&>(state);
         return evaluate_program(prog, envi);
     } else if (state.type == NullExpression) {
         return NullVal {
@@ -72,7 +72,7 @@ RunTimeVal evaluate(statement state, Environment& envi) {
             .value = NULL
         };
     } else if (state.type == VarDeclaraion) {
-        AST_VarDeclaraion var = state;
+        AST_VarDeclaraion var = static_cast<AST_VarDeclaraion&>(state);
         return eval_var_declaraion(var, envi);
     } else {
         return NullVal {

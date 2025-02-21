69545bf9f6453ea81407d3a82e3e810d57d0cc3e
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-02-21 15:39:43 +0700

    Delete test directory

--- test/Source/Mercury/envi.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/envi.h"
-
-RunTimeVal *Envi::MerEnvi_declare_var(string name, RunTimeVal *value) {
-    
-}
-
-RunTimeVal *Envi::MerEnvi_assign_var(string name, RunTimeVal *value) {
-       
-}

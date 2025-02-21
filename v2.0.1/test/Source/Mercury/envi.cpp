7a21c580b88cc24546301506d4a0b76010340ec1
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-02-21 16:07:24 +0700

    Delete v2.0.1/test directory

--- v2.0.1/test/Source/Mercury/envi.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/envi.h"
-
-RunTimeVal *Envi::MerEnvi_declare_var(string name, RunTimeVal *value, vector<Variable> *variables) {
-    Variable var = declared(name, value, variables);
-    return value;
-}
-

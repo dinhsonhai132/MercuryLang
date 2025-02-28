a1643ba414f044ce0f16486d89399af0ec6df848
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-02-28 22:16:02 +0700

    Add files via upload

---
+++ v2.0.1/test/source/include/eval.h
@@ -0,0 +1,17 @@
+#ifndef EVAL_H
+#define EVAL_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "envi.h"
+using namespace std;
+
+RunTimeVal MVM_valuelize(string type, float value);
+RunTimeVal MVM_evaluate(AST_node *node);
+RunTimeVal MVM_evaluate_binary_expression(AST_node *node);
+RunTimeVal MVM_evaluate_program(AST_node *node);
+RunTimeVal MVM_evaluate_expression_statement(AST_node *node);
+RunTimeVal MVM_evaluate_total_expression(AST_node *node);
+RunTimeVal MVM_evaluate_AST(AST_node *node);
+
+#endif // EVAL_H
---
+++ v2.0.1/test/source/parser/ast.cpp
@@ -0,0 +1,20 @@
+#include "./source/include/ast.h"
+
+AST_node *MerAST_new(void) {
+    return new AST_node();
+}
+
+AST_node *MerAST_make_parent(string type) {
+    AST_node *node = MerAST_new();
+    node->type = type;
+    return node;
+}
+
+AST_node *MerAST_make(string type, string tok, float value, string dt) {
+    AST_node *node = MerAST_new();
+    node->tok = tok;
+    node->value = value;
+    node->data_type = dt;
+    node->type = type;
+    return node;
+}

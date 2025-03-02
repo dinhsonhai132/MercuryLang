592b8035da51053e237daaf8c113e519a1b267fc
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-03-02 21:02:22 +0700

    Add files via upload

---
+++ v2.0.1/MerCompiler/ast.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+#define Program "Program"
+#define PrintStatement "PrintStatement"
+#define LetStatement "LetStatement"
+#define ExpressionStatement "ExpressionStatement"
+#define BlockStatement "BlockStatement"
+#define ReturnStatement "ReturnStatement"
+#define Expression "Expression"
+#define FunctionStatement "FunctionStatement"
+#define FunctionCallExpression "FunctionCallExpression"
+#define CallExpression "CallExpression"
+#define Identifier_ "Identifier"
+#define Literal "Literal"
+#define IntegerLiteral "IntegerLiteral"
+#define StringLiteral "StringLiteral"
+#define BinaryExpression "BinaryExpression"
+#define StringExpression "StringExpression"

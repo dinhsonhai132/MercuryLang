cba9f728745a9049f724cc2a350ec2707464992b
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-03-02 12:51:51 +0700

    Add files via upload

---
+++ v2.0.1/test/source/parser/buffer.cpp
@@ -0,0 +1,28 @@
+#include "../include/buffer.h" 
+
+char *MerBuffer_read_file_source(string file_name, string &source) {
+    char *buffer = NULL;
+    FILE *f = fopen(file_name.c_str(), "rb");
+    if (f == NULL)
+        return NULL;
+    fseek(f, 0, SEEK_END);
+    size_t length = ftell(f);
+    rewind(f);
+    buffer = new char[length + 1];
+    fread(buffer, 1, length, f);
+    buffer[length] = '\0';
+    fclose(f);
+    source = buffer;
+    return buffer;
+}
+
+char *MerBuffer_read_file(string file_name) {
+    string source;
+    return MerBuffer_read_file_source(file_name, source);
+}
+
+void MerBuffer_free(char *buffer) {
+    if (buffer == NULL) { cerr << FAILURE; return; };
+    delete[] buffer;
+    cerr << SUCCESS;
+}

c818377c87369534f3513ffbf4d251a501e42b8a
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-03-05 18:53:40 +0700

    Delete v2.0.1/Mercury directory

--- v2.0.1/Mercury/scr/buffer.h
+++
@@ -1,9 +0,0 @@
-#ifndef MERCURY_BUFFER_HEADER_FILE
-#define MERCURY_BUFFER_HEADER_FILE
-#include <string>
-using namespace std;
-
-char* MerBuffer_read_file_source(string file_name, string &source);
-char* MerBuffer_read_file(string file_name);
-
-#endif // MERCURY_BUFFER_HEADER_FILE

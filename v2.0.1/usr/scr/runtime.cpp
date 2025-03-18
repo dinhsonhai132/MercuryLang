3b0b84a333a1c5dd35dc6a45c042d0905fa6b22e
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-03-18 10:36:16 +0700

    Delete v2.0.1 directory

--- v2.0.1/usr/scr/runtime.cpp
+++
@@ -1,8 +0,0 @@
-#include "runtime.h"
-
-RunTimeVal valuelize(string type, float value) {
-    return RunTimeVal{value, type};
-}
-RunTimeVal *valuelize_as_ptr(string type, float value) {
-    return new RunTimeVal{value, type};
-}

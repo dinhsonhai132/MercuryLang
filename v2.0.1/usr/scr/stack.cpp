7733ac899717a5fd6cdb6bfeb3a9c9dfb292d62d
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-03-27 20:42:34 +0700

    Add files via upload

---
+++ v2.0.1/usr/scr/stack.cpp
@@ -0,0 +1,72 @@
+#include "stack.h"
+
+stack *MerCompiler_Stack_new(void) {
+    stack *s = new stack;
+    s->code = "";
+    s->cvalue = 0;
+    s->iden = "";
+    s->err = "";
+    s->s_table = MerCompiler_Table_new();
+    s->s_symtable = MerCompiler_SymbolTable_new();
+    return s;
+}
+table *MerCompiler_Table_new(void) {
+    table *t = new table;
+    t->cval = 0;
+    t->cidx = 0;
+    t->cbuf = 0;
+    t->err = "";
+    t->table = vector<table*>();
+    return t;
+}
+symtable *MerCompiler_SymbolTable_new(void) {
+    symtable *s = new symtable;
+    s->name = "";
+    s->value = 0;
+    s->type = "";
+    s->err = "";
+    s->pval = MerCompiler_Table_new();
+    s->cidx = 0;
+    s->address = 0x01;
+    s->stack = vector<symtable*>();
+    return s;
+}
+
+int MerCompiler_free_stack(stack *s) {
+    if (!s) return __FAILURE__;
+    delete[] s;
+    return __SUCCESS__;
+}
+int MerCompiler_free_table(table *t) {
+    if (!t) return __FAILURE__;
+    delete[] t;
+    return __SUCCESS__;
+}
+int MerCompiler_free_symboltable(symtable *s) {
+    if (!s) return __FAILURE__;
+    delete[] s;
+    return __SUCCESS__;
+}
+
+stack *MerCompiler_stack_setup(stack *s, table *t, symtable *sy) {
+    if (!s || !t || !sy) return NULL;
+    s = MerCompiler_Stack_new();
+    s->s_table = t;
+    s->s_symtable = sy;
+    return s;
+}
+
+table *MerCompiler_table_setup(float cval)
+{
+    table *t = MerCompiler_Table_new();
+    t->cval = cval;
+    return t;
+}
+
+symtable *MerCompiler_symboltable_setup(string name, int value, string type, uint8_t address = 0x0000) {
+    symtable *s = MerCompiler_SymbolTable_new();
+    s->name = name;
+    s->value = value;
+    s->type = type;
+    return s;
+}

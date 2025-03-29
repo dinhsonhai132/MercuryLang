9be2f7de07fe92fb7ec68f6aa678c85a53018a17
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-03-29 18:27:41 +0700

    Add files via upload

---
+++ v2.0.1/usr/main1.cpp
@@ -0,0 +1,129 @@
+#include "./scr/compile.cpp"
+#include "./scr/ceval.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+#define VERSION "2.0.1"
+#define AUTHOR  "Dinh Son Hai"
+
+#define COLOR_RESET   "\033[0m"
+#define COLOR_RED     "\033[31m"
+#define COLOR_GREEN   "\033[32m"
+#define COLOR_YELLOW  "\033[33m"
+#define COLOR_BLUE    "\033[34m"
+#define COLOR_CYAN    "\033[36m"
+#define COLOR_BOLD    "\033[1m"
+
+vector<char*> code_address;
+
+void print_help() {
+    cout << COLOR_BOLD << "MercuryLang CLI - " << VERSION << COLOR_RESET << endl;
+    cout << "Usage: " << COLOR_YELLOW << "mercury [options] <filename>.mer" << COLOR_RESET << endl;
+    cout << "Options:\n";
+    cout << "  " << COLOR_GREEN << "--version" << COLOR_RESET << "\t\tShow version information\n";
+    cout << "  " << COLOR_BLUE << "--prompt" << COLOR_RESET << "\t\tStart the Mercury REPL\n";
+    cout << "  " << COLOR_CYAN << "--help" << COLOR_RESET << "\t\tShow this help message\n";
+    cout << "  " << COLOR_RED << "--output <filename>" << COLOR_RESET << "\tCompile to specific output file\n";
+}
+
+void print_logo() {
+    cout << COLOR_BOLD << COLOR_CYAN;
+    cout << " __       __                                                              __                                     \n";
+    cout << "/  \\     /  |                                                            /  |                                    \n";
+    cout << "$$  \\   /$$ |  ______    ______    _______  __    __   ______   __    __ $$ |        ______   _______    ______  \n";
+    cout << "$$$  \\ /$$$ | /      \\  /      \\  /       |/  |  /  | /      \\ /  |  /  |$$ |       /      \\ /       \\  /      \\ \n";
+    cout << "$$$$  /$$$$ |/$$$$$$  |/$$$$$$  |/$$$$$$$/ $$ |  $$ |/$$$$$$  |$$ |  $$ |$$ |       $$$$$$  |$$$$$$$  |/$$$$$$  | \n";
+    cout << "$$ $$ $$/$$ |$$    $$ |$$ |  $$/ $$ |      $$ |  $$ |$$ |  $$/ $$ |  $$ |$$ |       /    $$ |$$ |  $$ |$$ |  $$ | \n";
+    cout << "$$ |$$$/ $$ |$$$$$$$$/ $$ |      $$ \\_____ $$ \\__$$ |$$ |      $$ \\__$$ |$$ |_____ /$$$$$$$ |$$ |  $$ |$$ \\__$$ | \n";
+    cout << "$$ | $/  $$ |$$       |$$ |      $$       |$$    $$/ $$ |      $$    $$ |$$       |$$    $$ |$$ |  $$ |$$    $$ | \n";
+    cout << "$$/      $$/  $$$$$$$/ $$/        $$$$$$$/  $$$$$$/  $$/        $$$$$$$ |$$$$$$$$/  $$$$$$$/ $$/   $$/  $$$$$$$ | \n";
+    cout << "                                                               /  \\__$$ |                              /  \\__$$ | \n";
+    cout << "                                                               $$    $$/                               $$    $$/  \n";
+    cout << "                                                                $$$$$$/                                 $$$$$$/   \n";
+    cout << COLOR_RESET;
+    cout << " MercuryLang version: " << VERSION << " - By " << AUTHOR << endl;
+}
+
+
+void prompt() {
+    print_logo();
+    cout << COLOR_BOLD << "Type 'exit' to quit\n" << COLOR_RESET;
+
+    string input;
+    while (true) {
+        cout << COLOR_YELLOW << ">>> " << COLOR_RESET;
+        getline(cin, input);
+        if (input == "exit") break;
+
+        vector<token> tokens = MerLexer_tokenize(input);
+        AST_node *ast = MerAST_make_AST(tokens);
+        char* code = MVM_compile_program(ast, code_address);
+        stack *stk = MerCompiler_Stack_new();
+        stk = MerCompiler_evaluate_program(code, stk);
+
+        for (auto &item : stk->s_table->table) {
+            cout << COLOR_GREEN << item->cval << COLOR_RESET << endl;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        print_help();
+        return EXIT_FAILURE;
+    }
+
+    string arg = argv[1];
+    bool output = false;
+    string output_file_name;
+
+    if (arg == "--version") {
+        cout << "MercuryLang Version " << VERSION << " by " << AUTHOR << endl;
+        return EXIT_SUCCESS;
+    } 
+    if (arg == "--prompt") {
+        prompt();
+        return EXIT_SUCCESS;
+    }
+    if (arg == "--help") {
+        print_help();
+        return EXIT_SUCCESS;
+    } 
+    if (arg == "--output") {
+        if (argc < 4) {
+            cerr << COLOR_RED << "Error: Output file not specified!" << COLOR_RESET << endl;
+            return EXIT_FAILURE;
+        }
+        arg = argv[2];
+        output_file_name = argv[3];
+        output = true;
+    }
+
+    size_t dot_pos = arg.find_last_of(".");
+    if (dot_pos == string::npos || arg.substr(dot_pos + 1) != "mer") {
+        cerr << COLOR_RED << "Error: Input file must have a .mer extension!" << COLOR_RESET << endl;
+        return EXIT_FAILURE;
+    }
+
+    string inputFile = arg;
+    string outputFile = output ? output_file_name + "merc-250.merc" : inputFile.substr(0, dot_pos) + "merc-250.merc";
+
+    try {
+        string source = MerBuffer_read_file(inputFile);
+        vector<token> tokens = MerLexer_tokenize(source);
+        AST_node *ast = MerAST_make_AST(tokens);
+        char* code = MVM_compile_program(ast, code_address);
+        MerBuffer_make_and_write_file(outputFile.c_str(), code);
+        
+        cout << COLOR_GREEN << "✅ Compilation successful! Output: " << outputFile << COLOR_RESET << endl;
+    } catch (const exception &e) {
+        cerr << COLOR_RED << "Compilation Error: " << e.what() << COLOR_RESET << endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ v2.0.1/usr/scr/bceval.h
@@ -0,0 +1,26 @@
+#ifndef MECURY_EVALUATE_H
+#define MECURY_EVALUATE_H
+
+#include "bctokenize.cpp"
+#include "stack.cpp"
+#include <stdlib.h>
+using namespace std;
+
+#define MERCURY_BINARY_SUB(c1, c2) ((c1) - (c2))
+#define MERCURY_BINARY_ADD(c1, c2) ((c1) + (c2))
+#define MERCURY_BINARY_DIV(c1, c2) ((c2) != 0 ? ((c1) / (c2)) : 0)
+#define MERCURY_BINARY_MUL(c1, c2) ((c1) * (c2))
+
+stack *MerCompilerBytecode_evaluate_program(vector<uint8_t> code);
+stack *MerCompilerBytecode_evaluate_statement(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_POP_NORMAL_MODE(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_PUSH_FLOAT(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_LOAD_GLOBAL(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_STORE_GLOBAL(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_BINARY_ADD(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_BINARY_DIV(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_BINARY_MUL(ctoken ptoken, stack *stk);
+stack *MerCompilerBytecode_evaluate_BINARY_SUB(ctoken ptoken, stack *stk);
+
+#endif //MECURY_EVALUATE_H

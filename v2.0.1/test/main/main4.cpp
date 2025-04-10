c73bd2e5713dc6888d2c183ac9dc1a5d6aef791c
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-04-10 17:55:45 +0700

    Add files via upload

---
+++ v2.0.1/test/main/main4.cpp
@@ -0,0 +1,79 @@
+#include "C:\MercuryLang-main\v2.0.1\mercury\ceval.cpp"
+
+#include <cstring>
+
+using namespace std;
+
+void print_ast(mAST_T *ast, int depth = 0) {
+    if (!ast) return;
+
+    string indent(depth * 2, ' ');
+    cout << indent << "{" << endl;
+
+    cout << indent << "  Node Type: " << ast->type << endl;
+
+    if (!ast->tok.empty()) {
+        cout << indent << "  Token: " << ast->tok << endl;
+    }
+    if (!ast->string_iden.empty()) {
+        cout << indent << "  Identifier: " << ast->string_iden << endl;
+    }
+    if (ast->value != 0.0) {
+        cout << indent << "  Value: " << ast->value << endl;
+    }
+
+    if (ast->type == LetStatement) {
+        cout << indent << "  Variable Name: " << ast->var_name << endl;
+        if (ast->var_value) {
+            cout << indent << "  Variable Value:" << endl;
+            print_ast(ast->var_value, depth + 1);
+        }
+    } else if (ast->type == FunctionStatement) {
+        cout << indent << "  Function Name: " << ast->func_name << endl;
+        cout << indent << "  Arguments:" << endl;
+        for (const auto &arg : ast->args_idens) {
+            cout << indent << "    " << arg << endl;
+        }
+        cout << indent << "  Body:" << endl;
+        for (auto &child : ast->body) {
+            print_ast(child, depth + 1);
+        }
+    } else if (ast->type == BinaryExpression) {
+        cout << indent << "  Operator: " << ast->op << endl;
+        cout << indent << "  Left:" << endl;
+        print_ast(ast->left, depth + 1);
+        cout << indent << "  Right:" << endl;
+        print_ast(ast->right, depth + 1);
+    }
+
+    if (!ast->children.empty()) {
+        cout << indent << "  Children:" << endl;
+        for (auto &child : ast->children) {
+            print_ast(child, depth + 1);
+        }
+    }
+
+    cout << indent << "}" << endl;
+}
+
+int main() {
+    const char* buff = "++  +";
+    mLexer_T *lexer = _MerLexer_init(buff);
+    mToken_T *token = _MerLexer_get_next_tok(lexer);
+    cout << token->tok << endl;
+    token = _MerLexer_get_next_tok(lexer);
+    cout << token->tok << endl;
+    token = _MerLexer_get_next_tok(lexer);
+    cout << token->tok << endl;
+    // mParser_T *parser = _MerParser_init(lexer);
+    // mAST_T *ast = MerParser_parse_program(parser);
+    // print_ast(ast);
+
+    // vector<uint8_t> code = MerCompiler_compile_ast_program(ast);
+    // stack *stk = MerCompilerBytecode_evaluate_program(code);
+
+    // for (auto &item : stk->s_table->table) {
+    //     cout << item->cval << endl;
+    // }
+    return 0;
+}

86fbe9ff316a34836f02b348cd4ac265595a94a8
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-04-20 22:36:49 +0700

    Delete v2.0.1 directory

--- v2.0.1/mercury/code.cpp
+++
@@ -1,9 +0,0 @@
-#include "C:\MercuryLang\v2.0.1\mercury\include\code.h"
-
-mCode_T Mer_push_code(mCode_T &code, Mer_uint8_t opcode) {
-    return NULL_CODE;
-}
-
-mCode_T Mer_push_out_code(mCode_T &code, Mer_uint8_t opcode) {
-    return NULL_CODE;
-}
--- v2.0.1/mercury/include/code.h
+++
@@ -1,9 +0,0 @@
-#include "C:\MercuryLang\v2.0.1\mercury\include\object.h"
-
-#ifndef MERCURY_CODE_HEADER_FILE
-#define MERCURY_CODE_HEADER_FILE
-
-mCode_T Mer_push_code(mCode_T &code, Mer_uint8_t opcode);
-mCode_T Mer_push_out_code(mCode_T &code, Mer_uint8_t opcode);
-
-#endif // MERCURY_CODE_HEADER_FILE

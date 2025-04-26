4f1f13b137228ceb0af6adcb5887af161608b1c1
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-04-26 19:23:38 +0700

    Delete v2.0.1 directory

--- v2.0.1/Mercury/code.cpp
+++
@@ -1,2 +0,0 @@
-#include "C:\MercuryLang\v2.0.1\mercury\include\code.h"
-
--- v2.0.1/Mercury/include/code.h
+++
@@ -1,7 +0,0 @@
-#include "C:\MercuryLang\v2.0.1\mercury\include\object.h"
-
-#ifndef MERCURY_CODE_HEADER_FILE
-#define MERCURY_CODE_HEADER_FILE
-
-
-#endif // MERCURY_CODE_HEADER_FILE

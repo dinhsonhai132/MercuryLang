e69834fe7672db73e56e4010ff0a8adb0747c6a0
Đinh Sơn Hải <dinhsonhai440@gmail.com>
2025-05-02 11:24:07 +0700

    Delete Mercury directory
    Signed-off-by: Đinh Sơn Hải <dinhsonhai440@gmail.com>

--- Mercury/buitlin.cpp
+++
@@ -1,8 +0,0 @@
-#include "./include/buitlin.h"
-
-MERCURY_API void __builtin_print(mObject_T *str) {
-    vector<Mer_uint8_t> buf = str->type->str->str->raw;
-    for (int i = 0; i < buf.size(); i++) {
-        fputc(mer_to_string(buf[i]), stdout);
-    }
-}
